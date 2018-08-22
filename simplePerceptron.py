b50790cbc39fbec7f0f7fd9d83cf0327e251fdea
zschuster <ztschuster@gmail.com>
2018-08-22 13:39:14 -0400

    Fit simple perceptron to Iris data

--- simplePerceptron.py
+++ simplePerceptron.py
@@ -54,36 +54,19 @@ class Perceptron(object):
         for _ in range(self.n_iter):
             errors = 0
             for xi, target in zip(X, y):
-                update = self.eta * (target * self.predict(xi))
+                update = self.eta * (target - self.predict(xi))
                 self.w_[1:] += update * xi
                 self.w_[0] += update
                 errors += int(update != 0.0)
             self.errors_.append(errors)
         return(self)
         
-        def net_input(self, X):
-            """Calculate net input"""
-            
-            return(np.dot(X, self.w_[1:]) + self.w_[0])
-            
-        def predict(self, X):
-            """Return class label after unit step"""
-            
-            return(np.where(self.net_input(X) >= 0.0, 1, -1))
-            
-
+    def net_input(self, X):
+        """Calculate net input"""
         
+        return(np.dot(X, self.w_[1:]) + self.w_[0])
         
+    def predict(self, X):
+        """Return class label after unit step"""
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-    
+        return(np.where(self.net_input(X) >= 0.0, 1, -1))
